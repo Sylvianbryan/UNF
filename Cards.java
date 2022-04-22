d39b0a26c9b9b511368261a0f34b16798c361e07
SWE <me@sylvianbryan.com>
2022-04-22 19:45:11 -0400

    Connection establish
    Manually enter username and password

---
+++ Cards.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class Test {
+
+    public static void main(string[] arg)
+            throws SQLException {
+        DriverManager.registerDriver((Driver) new com.mysql.cj.jdbc.Driver());
+
+        Connection conn = DriverManager.getConnection("jdbc:mysql://cisvm-winsrv-mysql1.unfcsd.unf.edu:3306/team2",
+                "N01422856", "Spring202234562856");
+
+        var stmt = conn.createStatement();
+        ResultSet rset = stmt.executeQuery("show tables");
+        while (rset.next())
+            System.out.println(rset.getString(1));
+    }
+}
