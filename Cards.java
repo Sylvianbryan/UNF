63f3342fc2d45320ebd1bae375585cb263e63769
SWE <me@sylvianbryan.com>
2022-04-23 00:10:12 -0400

    Update Cards.java
    Updated Java code and detail of the functionality for the Procedure Class.

--- Cards.java
+++ Cards.java
@@ -1,4 +1,10 @@
+/*The program generate a grade for a collection of cards using a Procedure Statement. 
+In additin,  A Function Statement is created to generate an Average Grade from using entries.   
+Objective is to interact with the MySQL Database using Java and JDBC Library.
+Group:  Team2 Project 2,  N#,  N#, N#,  N#@*/  
+
 import java.sql.*;
+import java.util.Scanner; //import Scanner class 
 
 public class Test {
 
@@ -15,3 +21,25 @@ public class Test {
             System.out.println(rset.getString(1));
     }
 }
+
+
+//Procedure Code Here -  
+
+public class grade {
+    /*User must be presented with Card and it information, upon recieving entry, the system loop to the next card in assembly fashion.  
+    Requesting Card Grade Entry from the user.*/
+    
+    /*SQL here to grab the Card Data from the Database Using Card ID*/ 
+    
+    /*Information is shown here about the card w/out image. Can we gather image of the front and back of the 11 card to generate imaes -  ask professor. */  
+    
+    Scanner Grade =  new Scanner (System.in);  /*Create Scanner object*/      
+    System.out.println("Enter Grade"); //Prompts user to enter their desired Grade.  
+    
+    /*Loop here to select the next card and repeat code above for entry  
+    
+}
+
+
+
+//Function Code Here. Vallue on Database requires default assignment of 0.  
